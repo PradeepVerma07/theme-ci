f606a2c476dee52d10350cbd0b1df8a37ecbe71f
Pradeep Verma <pradeepofficial8795@gmail.com>
2026-09-26 15:45:46 +0530

    Completely disable custom theme single layouts and render pure WordPress/Elementor post content for all single pages v2.5.9

--- single-ci_insight.php
+++ single-ci_insight.php
@@ -9,30 +9,13 @@ get_header();
 
 while ( have_posts() ) {
 	the_post();
-
-	$content      = get_the_content();
-	$is_elementor = false;
-
-	if ( class_exists( '\Elementor\Plugin' ) ) {
-		$document = \Elementor\Plugin::$instance->documents->get( get_the_ID() );
-		if ( $document && $document->is_built_with_elementor() ) {
-			$is_elementor = true;
-		}
-	}
-
-	if ( $is_elementor || ! empty( trim( (string) $content ) ) ) {
-		?>
-		<main id="main" <?php post_class( 'site-main' ); ?>>
-			<div class="page-content entry-content">
-				<?php the_content(); ?>
-			</div>
-		</main>
-		<?php
-	} else {
-		echo '<main id="main">';
-		echo ci_render_insight( get_the_ID() ); // phpcs:ignore WordPress.Security.EscapeOutput
-		echo '</main>';
-	}
+	?>
+	<main id="main" <?php post_class( 'site-main' ); ?>>
+		<div class="page-content entry-content">
+			<?php the_content(); ?>
+		</div>
+	</main>
+	<?php
 }
 
 get_footer();
