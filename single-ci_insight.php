4f4831a8bc27ec339aa6549a949a0cb0c56813b9
Pradeep Verma <pradeepofficial8795@gmail.com>
2026-09-26 18:07:57 +0530

    Set all blog posts, insights and case studies to render the unified modern template v2.7.1

--- single-ci_insight.php
+++ single-ci_insight.php
@@ -11,9 +11,15 @@ while ( have_posts() ) {
 	the_post();
 	?>
 	<main id="main" <?php post_class( 'site-main' ); ?>>
-		<div class="page-content entry-content">
-			<?php the_content(); ?>
-		</div>
+		<?php
+		if ( function_exists( 'ci_render_modern_blog_post' ) ) {
+			echo ci_render_modern_blog_post( get_the_ID() );
+		} else {
+			echo '<div class="page-content entry-content">';
+			the_content();
+			echo '</div>';
+		}
+		?>
 	</main>
 	<?php
 }
