b755da66ccec8e0c42d40466a4574e422b542cb0
Pradeep Verma <pradeepofficial8795@gmail.com>
2026-09-26 16:09:59 +0530

    Fix related articles grid to 4-in-a-row and enable full Elementor editing support on single posts v2.6.2

--- single.php
+++ single.php
@@ -12,11 +12,32 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 get_header();
 
-echo '<main id="main">';
 while ( have_posts() ) {
 	the_post();
-	echo ci_render_modern_blog_post( get_the_ID() );
+
+	$post_id      = get_the_ID();
+	$is_elementor = false;
+
+	if ( class_exists( '\Elementor\Plugin' ) ) {
+		$document = \Elementor\Plugin::$instance->documents->get( $post_id );
+		if ( $document && $document->is_built_with_elementor() ) {
+			$is_elementor = true;
+		}
+	}
+
+	if ( $is_elementor ) {
+		?>
+		<main id="main" <?php post_class( 'site-main' ); ?>>
+			<div class="page-content entry-content">
+				<?php the_content(); ?>
+			</div>
+		</main>
+		<?php
+	} else {
+		echo '<main id="main">';
+		echo ci_render_modern_blog_post( $post_id );
+		echo '</main>';
+	}
 }
-echo '</main>';
 
 get_footer();
